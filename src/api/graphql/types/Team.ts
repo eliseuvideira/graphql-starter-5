640177ba126654ab81e649d75e714fbc5da0e4ea
Eliseu Videira <eliseuvideira1@gmail.com>
2021-09-05 18:37:57 -0300

    :sparkles: layouts done

--- src/api/graphql/types/Team.ts
+++ src/api/graphql/types/Team.ts
@@ -7,41 +7,60 @@ import {
   GraphQLObjectType,
   GraphQLString,
 } from "graphql";
+import path from "path";
+import fs from "fs";
 import { TeamProps } from "../../../models/Team";
 import { GraphQLDateTime } from "../scalars/DateTime";
 
 export const Team = new GraphQLObjectType<TeamProps>({
   name: "Team",
-  fields: () => ({
-    teamId: {
-      type: new GraphQLNonNull(GraphQLID),
-    },
-    name: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    city: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    location: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    region: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    colors: {
-      type: new GraphQLNonNull(
-        new GraphQLList(new GraphQLNonNull(GraphQLString))
-      ),
-    },
-    active: {
-      type: new GraphQLNonNull(GraphQLBoolean),
-    },
-    createdAt: {
-      type: new GraphQLNonNull(GraphQLDateTime),
-    },
-    updatedAt: {
-      type: new GraphQLNonNull(GraphQLDateTime),
-    },
-    ...require("../fields/Edges/Team/players"),
-  }),
+  fields: () => {
+    let edges = {};
+
+    const EDGE_DIR = path.join(__dirname, "..", "fields", "Edge", "Team");
+
+    const edgesFiles = fs.readdirSync(EDGE_DIR);
+
+    for (const edge of edgesFiles) {
+      const EDGE_FILE = path.join(EDGE_DIR, edge);
+
+      const module = require(EDGE_FILE);
+
+      edges = { ...edges, ...module };
+    }
+
+    return {
+      teamId: {
+        type: new GraphQLNonNull(GraphQLID),
+      },
+      name: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      city: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      location: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      region: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      colors: {
+        type: new GraphQLNonNull(
+          new GraphQLList(new GraphQLNonNull(GraphQLString))
+        ),
+      },
+      active: {
+        type: new GraphQLNonNull(GraphQLBoolean),
+      },
+      createdAt: {
+        type: new GraphQLNonNull(GraphQLDateTime),
+      },
+      updatedAt: {
+        type: new GraphQLNonNull(GraphQLDateTime),
+      },
+
+      ...edges,
+    };
+  },
 });
